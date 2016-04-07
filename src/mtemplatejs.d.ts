428918bef30421b7bf341e9038d8c7b2a4e36332
mtoolkit <mtoolkit.framework@gmail.com>
2016-04-07 16:05:28 +0200

    Removed jQuery reference from mtemplatejs.d.ts

--- src/mtemplatejs.d.ts
+++ src/mtemplatejs.d.ts
@@ -1,8 +1,6 @@
 // Type definitions for jQuery MTemplateJS
 // Project: https://github.com/MpStyle/jquery-mtemplatejs
 
-/// <reference path="libs/jquery.d.ts"/>
-
 interface JQuery {
     mtemplatejs(data:JQuery, directives?:{ [key:string]:($item:JQuery, record:any)=>void }): JQuery;
 }
