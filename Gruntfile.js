5ca46a8cd4dc6b6f78d85e9179f262c65bd67986
mtoolkit <mtoolkit.framework@gmail.com>
2016-04-07 12:50:26 +0200

    Added license in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         ts: {
             default: {
                 files: [{src: ['src/mtemplatejs.ts'], dest: 'dist/jquery-mtemplatejs.js'}],
@@ -19,7 +20,16 @@ module.exports = function (grunt) {
             options: {
                 compress: {
                     drop_console: true
-                }
+                },
+                banner: '/* \n' +
+                '* jQuery MTemplateJS - v<%= pkg.version %> \n' +
+                '* Template library. \n' +
+                '* https://github.com/MpStyle/jquery-mtemplatejs \n' +
+                '* \n' +
+                '* Powered by Michele Pagnin \n' +
+                '* Licensed under a LGPL 3.0 license \n' +
+                '* http://www.gnu.org/licenses/lgpl-3.0.html \n' +
+                '*/ \n\n'
             }
         }
     });
